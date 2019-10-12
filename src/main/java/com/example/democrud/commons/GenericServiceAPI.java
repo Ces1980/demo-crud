660b06b34a249a765e2b8d9a85fb23611df568ee
César Vargas Escorcia <thecesarvargas@gmail.com>
2019-10-12 03:04:29 -0500

    Creando la clase GenericServiceImpl para implementar la interfaz GenericServiceAPI que pertenecen al paquete com.example.democrud.commons

--- src/main/java/com/example/democrud/commons/GenericServiceAPI.java
+++ src/main/java/com/example/democrud/commons/GenericServiceAPI.java
@@ -3,6 +3,8 @@ package com.example.democrud.commons;
 import java.io.Serializable;
 import java.util.List;
 
+
+
 /*+
  * Básicamente serializar es pasar un Objeto a un array de bytes y viceversa. 
  * Sirve para poder trasmitir y escribir objetos a medios externos como webservices, archivos.
@@ -10,6 +12,13 @@ import java.util.List;
  * */
 public interface GenericServiceAPI<T, ID extends Serializable> {
 
+	// La T que se declara dentro de la lista indica la entidad o el tipo
+	/*Todos los tipos (T) van a ser remplazados por las entidades de tipo persona*/
+	
+	/*Al ser usada esta interfaz mandandola llamar posteriormente en la interface PersonaServiceAPI
+	 * los valores van a ser sustituidos por  
+	 *<Persona = T, Long = entity>  */
+	
 	/*Método de guardar -> Guarda la entidad */
 	T save(T entity);
 	/*Método de borrar el cual recibe por parametro el id de la entidad*/
---
+++ src/main/java/com/example/democrud/commons/GenericServiceImpl.java
@@ -0,0 +1,68 @@
+package com.example.democrud.commons;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/*
+ * para poder utilizar este método --> public abstract CrudRepository<T, ID> getDao();
+ * se realiza la siguiente importación*/
+import org.springframework.data.repository.CrudRepository;
+
+/*
+ * Esta clase va a implementar la interfaz GenericServiceAPI<T, ID >
+ * De igual forma recibe por parametros la interfaz seializable -->
+ * Explicación de serializable en package com.example.democrud.commons
+ * dentro de la interfaz interface GenericServiceAPI  */
+
+/*
+ * El extends de Serializabledebe ser en la interfaz de la clase y no en la implementacion*/
+public abstract class GenericServiceImpl <T, ID extends Serializable> implements GenericServiceAPI<T, ID>  {
+
+	/*
+	 *Se implementan los métodos de la interfaz GenericServiceAPI */
+	
+	
+	/*Utilizando el método abstracto getDao() se complementan 
+	 * los métods llamados traer gracias a la implementacion
+	 * de la interfaz GenericServiceAPI */
+	
+	
+	@Override
+	public T save(T entity) {
+		
+		return getDao().save(entity);
+	}
+
+	@Override
+	public void delete(ID id) {
+		
+		getDao().deleteById(id);
+	}
+
+	@Override
+	public T get(ID id) {
+		Optional<T> obj = getDao().findById(id);
+		if(obj.isPresent()) {
+			return obj.get();
+		}
+		return null;
+	}
+
+	@Override
+	public List<T> getAll() {
+		List<T> returnList = new ArrayList<>();
+		getDao().findAll().forEach(obj -> returnList.add(obj));
+		return returnList;
+	}
+/*
+ * Se rrquiere crear un método que ayude a indicar a que repositorio
+ * se esta llamadno a solicitando (com.example.democrud.dao.api --> interface PersonaDaoApi) */
+	
+	public abstract CrudRepository<T, ID> getDao();
+}
+
+
+
+
